f17ba7640a32a77e8084a60b5efb82d38bfb1280
DrMugg <66801203+DrMugg@users.noreply.github.com>
2022-02-15 16:38:54 +0100

    Update numpy_regression.py
    FÄÄÄRDIIG

--- numpy_regression.py
+++ numpy_regression.py
@@ -2,7 +2,7 @@ import numpy as matrix
 import sys
 import matplotlib.pyplot as plt
 
-def makelist(x1,y1,z1,z2):
+def makelist(x1,y1,z1,z2): #skapar en lista från z1 till z2 enligt linjärekvation x1=m y1=k z1=x
 	x=[x1]
 	y=[y1]
 	while z1<z2:
@@ -26,12 +26,25 @@ def powers(lin,a,b):
 		lout.append(templist)
 	return matrix.array(lout)
 
+def poly(a, x, n):
+	out=[]
+	if len(a)<n:
+		n=len(a)
+	for i in range(0,len(x)):
+		temp=0
+		for exponent in range(0,n+1):
+			temp+=a[exponent]*x[i]**exponent
+		out.append(temp)
+	return out
+
 def main():
 	inputtxt="chirps.txt"
+	n=3
 	if len(sys.argv)>1:
+		n=sys.argv[len(sys.argv)-1]
 		inputtxt=sys.argv[1]
 		if len(sys.argv)>2:
-			n=sys.argv[2]
+			n=int(sys.argv[2])
 	xy=matrix.transpose(matrix.loadtxt(inputtxt))
 	X=xy[0]
 	Y=xy[1]
@@ -40,12 +53,12 @@ def main():
 	Xpt = matrix.transpose(Xp)
 	a= matrix.matmul(matrix.linalg.inv(matrix.matmul(Xpt,Xp)),matrix.matmul(Xpt,Yp))
 	a=a[:,0]
-
 	#implementera poly enligt uppgift och ändra x-värden
-	mt=makelist(b,m,1,len(X))
-	x = [min(X), max(X)]
-	y = [temperature(b, m, temp) for temp in x]
+	Xvarden=matrix.linspace(min(X),max(X),40).tolist()
+	Yvarden=poly(a,Xvarden,n)
+	x = [min(X), max(Xvarden)]
+	print(len(Yvarden),len(X))
 	plt.plot(X,Y,'ro')
-	plt.plot(x,y)
+	plt.plot(Xvarden,Yvarden)
 	plt.show()
 main()
